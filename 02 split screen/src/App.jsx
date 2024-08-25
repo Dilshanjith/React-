3e56c262edce606b4117b4b314d6bab77c4b819d
Dilshan samarasinghe <113366721+Dilshanjith@users.noreply.github.com>
2024-08-26 00:05:13 +0530

    split
    split screen

---
+++ 02 split screen/src/App.jsx
@@ -0,0 +1,19 @@
+import BodyContent from "./Components/BodyContent/BodyContent"
+import FooterContent from "./Components/FooterContent/FooterContent"
+import HeaderContent from "./Components/HeaderContent/HeaderContent"
+
+
+
+ function App() {
+  return (
+    <>
+      <div id="wrapper">
+        <HeaderContent />
+        <BodyContent />
+        <FooterContent />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 02 split screen/src/Components/FooterContent/FooterContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  './FooterContent.css'
+
+function FooterContent() {
+  return (
+    <div id="footercontent">
+       <p>Footer content</p>
+    </div>
+  )
+}
+
+export default FooterContent
